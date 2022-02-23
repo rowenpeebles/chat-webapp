9f31ca80819940848b5a4b6ed6ad7316d25302a5
Rowen Peebles <77802190+rowenpeebles@users.noreply.github.com>
2022-02-23 16:08:54 +0000

    Adding components, methods & functions to container.vue

--- chatapp/resources/js/Pages/Chat/container.vue
+++ chatapp/resources/js/Pages/Chat/container.vue
@@ -11,7 +11,7 @@
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                     <welcome />
                     <message-container />
-                    <input-message />
+                    <input-message :room="currentRoom" />
                 </div>
             </div>
         </div>
@@ -31,5 +31,40 @@
             MessageContainer,
             InputMessage
         },
+        data: function () {
+            return {
+                chatRooms: [],
+                currentRoom: [],
+                messages: []
+            }
+        },
+        methods: {
+            getRooms() {
+                axios.get('/chat/rooms')
+                .then( response => {
+                    this.chatRooms = response.data;
+                    this.setRoom( response.data[0] );
+                })
+                .catch( error => {
+                    console.log( error );
+                })
+            },
+            setRoom( room ) {
+                this.currentRoom = room;
+                this.getMessages();
+            },
+            getMessages() {
+                axios.get('/chat/room/' + this.currentRoom.id + '/messages')
+                .then( response => {
+                    this.messages = response.data;
+                })
+                .catch( error => {
+                    console.log( error );
+                })
+            }
+        },
+        created() {
+            this.getRooms();
+        }
     }
 </script>
